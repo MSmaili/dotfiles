52e33bd5cf26de94f22e5cebd0bdc285c81a3121
Muhamet Smaili <muhamet5@msn.com>
2026-01-10 17:14:04 +0200

    remove fugitive

--- .config/nvim/lua/custom/plugins/diff.lua
+++ .config/nvim/lua/custom/plugins/diff.lua
@@ -2,4 +2,7 @@ return {
 	"esmuellert/codediff.nvim",
 	dependencies = { "MunifTanjim/nui.nvim" },
 	cmd = "CodeDiff",
+	keys = {
+		{ "<leader>gs", "<cmd>CodeDiff<cr>", desc = "Git diff 3-way-split" },
+	},
 }
